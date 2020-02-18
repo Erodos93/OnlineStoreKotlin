982e3e29c5ae3f718e7ff0c5f156ad8cb50aea0b
Erodos93 <58087567+Erodos93@users.noreply.github.com>
2020-02-18 12:23:02 +0100

    Backup changes on the project

--- app/src/main/java/com/example/onlinestorekotlin/CartProductActivity.kt
+++ app/src/main/java/com/example/onlinestorekotlin/CartProductActivity.kt
@@ -16,34 +16,63 @@ class CartProductActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cart_product)
 
-        val cartUrl =
+        val cartProductUrl =
             "http://10.1.8.10/OnlineStoreApp/fench_teporary_order.php?email=${Person.email}"
-        var cartarrayList = ArrayList<TemporaryCart>()
+        var cartProductList = ArrayList<String>()
         var requestCart = Volley.newRequestQueue(this@CartProductActivity)
         var cartRequest =
-            JsonArrayRequest(Request.Method.GET, cartUrl, null, Response.Listener { response ->
-                for (jsonIndex in 0.until(response.length())) {
-                    var jsonArrayList = response.getJSONObject(jsonIndex)
+            JsonArrayRequest(
+                Request.Method.GET,
+                cartProductUrl,
+                null,
+                Response.Listener { response ->
+                    for (jsonIndex in 0.until(response.length())) {
+                        var jsonArrayList = response.getJSONObject(jsonIndex)
+                        cartProductList.add(
+                            "${response.getJSONObject(jsonIndex).getInt("id")}\n" +
+                                    "${response.getJSONObject(jsonIndex).getString("name")}\n" +
+                                    " ${response.getJSONObject(jsonIndex).getInt("price")}\n" +
+                                    " ${response.getJSONObject(jsonIndex).getString("email")}\n" +
+                                    " ${response.getJSONObject(jsonIndex).getInt("amount")}"
+                        )
+                        var adapter = ArrayAdapter(
+                            this@CartProductActivity,
+                            android.R.layout.simple_list_item_1, cartProductList
 
-                    cartarrayList.add(
-                        TemporaryCart(jsonArrayList.getInt("id"),
-                            jsonArrayList.getString("name"),
-                            jsonArrayList.getInt("price"),
-                            jsonArrayList.getString("email"),
-                            jsonArrayList.getInt("amount"))
+                        )
+                        listView.adapter = adapter
 
-                    )
-                }
-            }, Response.ErrorListener {
+                    }
+                }, Response.ErrorListener { error ->
 
-            })
-        requestCart.add(cartRequest)
-        var adapter = ArrayAdapter(
-            this@CartProductActivity,
-            android.R.layout.simple_list_item_1, cartarrayList
+                })
+        /* var cartarrayList = ArrayList<TemporaryCart>()
+         var requestCart = Volley.newRequestQueue(this@CartProductActivity)
+         var cartRequest =
+             JsonArrayRequest(Request.Method.GET, cartUrl, null, Response.Listener { response ->
+                 for (jsonIndex in 0.until(response.length())) {
+                     var jsonArrayList = response.getJSONObject(jsonIndex)
 
-        )
-        listView.adapter = adapter
+                     cartarrayList.add(
+                         TemporaryCart(jsonArrayList.getInt("id").toInt(),
+                             jsonArrayList.getString("name").toString(),
+                             jsonArrayList.getInt("price").toInt(),
+                             jsonArrayList.getString("email").toString(),
+                             jsonArrayList.getInt("amount").toInt())
+
+                     )
+                     var adapter = ArrayAdapter(
+                         this@CartProductActivity,
+                         android.R.layout.simple_list_item_1, cartarrayList
+
+                     )
+                     listView.adapter = adapter
+                 }
+             }, Response.ErrorListener {
+
+             })
+         requestCart.add(cartRequest)*/
+
 
     }
 }
